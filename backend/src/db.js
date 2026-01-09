a6d619a3f79c7de3b08d4d861c17a2fa79d0a070
bidave-omkar <omkarbidave.pro@gmail.com>
2026-01-09 15:56:39 +0530

    final fix

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,16 +1,16 @@
-// // src/db.js
-// import pkg from "pg";
-// const { Pool } = pkg;
-// import dotenv from "dotenv";
-// dotenv.config();
+// src/db.js
+import pkg from "pg";
+const { Pool } = pkg;
+import dotenv from "dotenv";
+dotenv.config();
 
-// export const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-//   ssl: {
-//     rejectUnauthorized: false // REQUIRED on Render
-//   }
-// });
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // REQUIRED on Render
+  }
+});
