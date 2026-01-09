5542a1ae2978a7b87c79fec293c90f2bb8431fda
bidave-omkar <omkarbidave.pro@gmail.com>
2026-01-09 15:50:03 +0530

    final fix

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,16 @@
-// src/db.js
-import pkg from "pg";
-const { Pool } = pkg;
-import dotenv from "dotenv";
-dotenv.config();
+// // src/db.js
+// import pkg from "pg";
+// const { Pool } = pkg;
+// import dotenv from "dotenv";
+// dotenv.config();
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || `postgresql://${process.env.PGUSER || "postgres"}:${process.env.PGPASSWORD || "password"}@${process.env.PGHOST || "localhost"}:${process.env.PGPORT || 5432}/${process.env.PGDATABASE || "smart_vehicle_db"}`,
-  // ssl: { rejectUnauthorized: false } // enable if using hosted DB requiring SSL
-});
+// export const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   ssl: {
+//     rejectUnauthorized: false // REQUIRED on Render
+//   }
+// });
